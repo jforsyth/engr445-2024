818f1c08ebf4731281043d351d5b821ce3fc8ddf
Jason Forsyth <forsy2jb@jmu.edu>
2024-11-12 16:01:02 -0500

    Replace with updated version

---
+++ milestones/step-counting/App.cpp
@@ -0,0 +1,221 @@
+#include "App.h"
+#include <cmath>
+using namespace std;
+
+bool splits(std::string line, std::vector<std::string>& container)
+{
+    std::vector<std::string>& parts = container;
+
+    int start_pos=0;
+    int end_pos=-1;
+
+    while(start_pos!=-1)
+    {
+        end_pos = line.find_first_of(',');
+        if (end_pos==-1 && !line.empty())
+        {
+            parts.push_back(line);
+            return true;
+        }
+        std::string sub_string = line.substr(start_pos,end_pos);
+        parts.push_back(sub_string);
+        line = line.substr(end_pos+1,line.length()-1);
+
+        if(line.empty())
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+/**
+ * Define all global variables and data arrays
+ */
+const int ARRAY_LENGTH = 16;
+float magnitude[ARRAY_LENGTH];
+float diffs[ARRAY_LENGTH];
+float squares[ARRAY_LENGTH];
+float averages[ARRAY_LENGTH];
+
+/**
+ * Enumerate some basic states the machine may wish to follow
+ */
+enum STATE {IDLE, DETECT, TIMEOUT};
+STATE currentState;
+STATE nextState;
+
+/**
+ * Create a file input stream that we will constantly read from.
+ */
+ifstream fin;
+bool app_init(void)
+{
+    /**
+     * Step 0: Initial all arrays defined above to known values. Really should be 0.
+     * Will use FOR LOOP that is very inefficient but is guaranteed to work on all systems
+     */
+
+    for(int i=0;i<ARRAY_LENGTH;i++)
+    {
+        magnitude[i]=0;
+        diffs[i]=0;
+        squares[i]=0;
+        averages[i]=0;
+    }
+
+    // Initialize the states of the machine to the initial one
+    currentState = IDLE;
+    nextState = IDLE;
+
+    /**
+     * Step 1: Initialize any data sources that will be needed. When running on PC, this will be the
+     * .csv files that contain step data. On the Giant Gecko this will be accessing acceleration data from ADXL345.
+     */
+    string filePath = "p1.1_Female_20-29_170-179cm_Hand_held.out.csv";
+    filePath="../"+filePath;
+    fin.open(filePath.c_str());
+
+    if(fin.fail())
+    {
+        cout << "Failed opening file: " << filePath << endl;
+        cout << "Could not open file! Check file path. Consider using an absolute path.";
+        return false;
+    }
+
+    // Proceed to read out the first line of the file in case it holds header information
+    string header="";
+    fin >> header;
+    cout << "Disposing of header: " << header << endl;
+
+    /**
+     * Step 2: Initialize any other system level operations: Timers, LCD screen...etc
+     */
+    // do nothing at this time
+
+    return true;
+}
+
+/**
+ * Create a counter variable to track how ofter this loop is called. On the PC, this will correspond to each line
+ * of the text file. On the Giant Gecko, this will be each "tick" of the program at each sample.
+ */
+int counter = 0;
+bool app_process_action(void)
+{
+    /**
+     * Step 1: Read data from the source. In this example we will use the file input stream FIN to read from the
+     * previously defined CSV file.
+     */
+    if(!fin.good())
+    {
+        cout << "We are at the end of file or an error occurred. Exiting." << endl;
+        return false;
+    }
+
+    // read from file into string
+    string line = "";
+    fin >> line;
+
+    // check for blank line at end of file...
+    if (line=="")
+    {
+        return false;
+    }
+
+
+    // now split the string based upon comma separators. Will populate a vector that contains
+    // all the elements of the line. Vector length adjusts based upon number of columns
+    vector<string> line_parts;
+    bool success = splits(line,line_parts);
+
+    /**
+         * Pull out the X, Y, and Z acceleration and convert to floats.
+         * This can be expanded for other data types in the file
+         * Each column, for this particular row, now resides as elements of the string
+         * Time = [0]
+         * AccelX = [1]
+         * AccelX = [2]
+         * AccelX = [3]
+         * GyroX = [4]
+         * ... etc
+         *
+         * If you are operating on a file with a different structure, adjust the values
+         * 1, 2, and 3 below to be the columns that hold the X, Y, and Z acceleration.
+         * 0 is the "first column", 1 is the "second"..etc.
+         */
+    float x = atof(line_parts[1].c_str());
+    float y = atof(line_parts[2].c_str());
+    float z = atof(line_parts[3].c_str());
+
+    /**
+     * Step 2: Begin data processing pipeline. The first action is likely to calculate the magnitude
+     * of the most recent sample.
+     */
+    float mag = sqrt(x*x + y*y + z*z);
+
+
+
+    /**
+     * Step 3: Utilize finite state machine to determine whether a step has been detected.
+     */
+
+    switch(currentState){
+
+    case(IDLE):
+        // do what is needed in the IDLE state. We'll just stay here as an example
+
+        /**
+         * Implement the STATE actions
+         */
+
+
+        /**
+         * Implement the STATE nextState logic
+         */
+
+        break;
+
+    case(DETECT):
+        // do what is needed in the DETECT state
+
+        /**
+        * Implement the STATE actions
+        */
+
+
+        /**
+         * Implement the STATE nextState logic
+         */
+        break;
+
+    case(TIMEOUT):
+        // do what is needed in the TIMEOUT state
+
+        /**
+        * Implement the STATE actions
+        */
+
+
+        /**
+         * Implement the STATE nextState logic
+         */
+        break;
+
+    default:
+        cout << "Error! This should never happen!" << endl;
+        break;
+    }
+
+
+    // update state machine for next iteration
+    currentState = nextState;
+
+    // increment our counter to know how many iterations this has been.
+    counter++;
+
+    // return true so the main() method will continue to call us...
+    return true;
+}
--- milestones/step-counting/main.cpp
+++ milestones/step-counting/main.cpp
@@ -1,202 +1,29 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
-
+#include "App.h"
 using namespace std;
 
-/**
- * Dr. Forsyth's code for parsing a string...
- * @param line The string to be parsed
- * @param container A vector<string> container to hold the results
- * @return Return True if we successfully complete. False otherwise.
- */
-bool splits(string line, vector<string> &container) {
-    vector<string> &parts = container;
-
-    int start_pos = 0;
-    int end_pos = -1;
-
-    while (start_pos != -1) {
-        end_pos = line.find_first_of(',');
-        if (end_pos == -1 && !line.empty()) {
-            parts.push_back(line);
-            return true;
-        }
-        string sub_string = line.substr(start_pos, end_pos);
-        parts.push_back(sub_string);
-        line = line.substr(end_pos + 1, line.length() - 1);
-
-        if (line.empty()) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
 int main() {
-    //create the path to the file. You can the name to access different files
-    string filePath = "48_steps.csv";
+    cout << "Booting up your Program...Calling init() function" << endl;
 
-    // since executing from debug, point up a directory.
-    // Don't change this unless you know what you're doing...
-    filePath = "../" + filePath;
+    bool init_success = app_init();
 
-    //create an input file stream to hold the file
-    ifstream in;
-
-    //open the file
-    in.open(filePath.c_str());
-
-    if (in.fail()) {
-        cout << "Failed opening file: " << filePath << endl;
-        cout << "Could not open file! Check file path. Consider using an absolute path.";
+    if (!init_success) {
+        cout << "Initialization failed. Program will not proceed. Exiting." << endl;
         return -1;
     }
 
+    cout << "Initialization success. Will now proceed to called your process_action() function repeatedly.." << endl;
+    int LOOP_ACTION_LIMIT = 1000000;
+    for (int i = 0; i < LOOP_ACTION_LIMIT; i++) {
+        bool success = app_process_action();
 
-    /**
-     * Create containers to hold the accel X, Y, and Z data.
-     * Each time through the loop one sample will be added to each vector<>
-     */
-    vector<float> accelX;
-    vector<float> accelY;
-    vector<float> accelZ;
-
-    vector<float> magnitude;
-    vector<float> diff;
-    vector<float> square;
-    vector<float> moving_average;
-
-    const int N = 10;
-
-
-    /**
-     * Create any initial global variables your program might need...
-     */
-    enum State {
-        INIT, STATE_A, STATE_B, STATE_C
-    };
-    State currentState = INIT;
-    State nextState = INIT;
-    int threshold = -1;
-
-    // track whether the datapath has been initialized. If not, do not perform operations on it
-    bool initialized = false;
-
-    //while there's data in the file...
-    while (in.good())
-    {
-        if (initialized)
-        {
-            //read in a single line
-            string line = "";
-            in >> line;
-
-            //ignore any blank lines
-            if (line.empty()) {
-                continue;
-            }
-
-            /**
-            * Break the line into pieces based upon ',' and convert elements to integers
-            */
-            vector<string> subParts;
-            bool success = splits(line, subParts);
-
-            /**
-             * Convert elements of string into numbers (floats for this dataset)
-             */
-
-            // ignore the header information or quick if fails
-            if (subParts[0] == "time" || !success) {
-                continue;
-            }
-
-            /**
-             * Pull out the X, Y, and Z acceleration and convert to floats.
-             * This can be expanded for other data types in the file
-             * Each column, for this particular row, now resides as elements of the string
-             * Time = [0]
-             * AccelX = [1]
-             * AccelX = [2]
-             * AccelX = [3]
-             * GyroX = [4]
-             * ... etc
-             *
-             * If you are operating on a file with a different structure, adjust the values
-             * 1, 2, and 3 below to be the columns that hold the X, Y, and Z acceleration.
-             * 0 is the "first column", 1 is the "second"..etc.
-             */
-            float x = atof(subParts[0].c_str());
-            float y = atof(subParts[1].c_str());
-            float z = atof(subParts[2].c_str());
-
-            /**
-             * Take the data from the single line and add it into our own vector
-             */
-            accelX.push_back(x);
-            accelY.push_back(y);
-            accelZ.push_back(z);
-
-            /**
-             *  The data arrays should now be a fixed length. Perform a test to see
-             *  if the vector is "too long" and then drop the first element. All vectors will
-             *  need to be a fixed length on the board, otherwise, memory will be exhausted.
-             */
-            if (accelX.size() > N) {
-                accelX.erase(accelX.begin());
-                accelY.erase(accelY.begin());
-                accelZ.erase(accelZ.begin());
-            }
-
-            // comment the line below to quiet all the data coming out...
-            cout << "X: " << x << "\tY:" << y << "\tZ: " << z << endl;
-
-            /**
-             * BEGIN YOUR DATAPATH CODE HERE
-             */
-
-            //your code here
+        if (!success) {
+            cout << "App action exiting..." << endl;
+            return -1;
         }
-
-        /**
-         * BEING YOUR FINITE STATE MACHINE CODE HERE
-         */
-
-        // your code here
-        switch (currentState) {
-            case INIT:
-                /**
-                 * Use this state to initialize all vectors/arrays to the best length.
-                 * Assume the acceleration vectors are of length N
-                 */
-                for (int i = 0; i < N; i++) {
-                    accelX.push_back(0);
-                    accelY.push_back(0);
-                    accelZ.push_back(0);
-                    magnitude.push_back(0);
-                }
-
-                /**
-                 * Assume the derived vectors are of length N-1
-                 */
-                for (int i = 0; i < N - 1; i++) {
-                    diff.push_back(0);
-                    square.push_back(0);
-                    moving_average.push_back(0);
-                }
-
-                // datapath is initialized. We can now sample data and process it
-                initialized=true;
-                break;
-        }
-
     }
 
-    /**
-     * Report the number of steps counted over all...
-     */
+    cout << "This should never happen. main() has called your code " << LOOP_ACTION_LIMIT << " times." << endl;
 
-
+    return -1;
 }
